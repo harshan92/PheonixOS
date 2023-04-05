2acdf060a224a25273a37a2efe1b11f2f4c22f3b
HM2-WIMALASIRI <harshanmadhurangaw@gmail.com>
2023-04-05 20:04:31 +0530

    added background colour

--- boot/boot.asm
+++ boot/boot.asm
@@ -3,12 +3,25 @@
 
 section code
 
-.main:
+.init:
     mov eax, 0xb800
     mov es, eax
+    mov eax, 0 ; set eax to 0 ->i=0
 
+
+
+.clear:
+    mov byte [es:eax], 0; move blank charactors to current text
+    inc eax
+    mov byte [es:eax], 0x30; move the background color and color to the next address
+    inc eax
+    cmp eax, 2*25*80
+
+    jl .clear
+
+.main:
     mov byte [es:0x00], 'H'; 0xb800 + 0x00 = 0xb800
-    mov byte [es:0x01], 0x40
+    mov byte [es:0x01], 0x30
 
 jmp $
 
