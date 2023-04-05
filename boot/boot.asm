30b0fa6ded815d05a88605d2142477a472f2443d
HM2-WIMALASIRI <harshanmadhurangaw@gmail.com>
2023-04-05 19:27:37 +0530

    implementet boot file

--- boot/boot.asm
+++ boot/boot.asm
@@ -4,3 +4,15 @@
 section code
 
 .main:
+    mov eax, 0xb800
+    mov es, eax
+
+    mov byte [es:0x00], 'H'; 0xb800 + 0x00 = 0xb800
+    mov byte [es:0x01], 0x30
+
+jmp $
+
+times 510 - ($-$$) db 0x00 ; pads the file with 0s, making it right size
+
+db 0x55
+db 0xaa
