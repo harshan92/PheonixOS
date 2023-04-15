b30206001797d3a4cc182e605141c09f3518e898
HM2-WIMALASIRI <harshanmadhurangaw@gmail.com>
2023-04-15 08:42:57 +0530

    switching protected mode

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,11 +1,9 @@
-[org 0x00]
+[org 0x7c00]
 [bits 16]
 
 section code
 
 .init:
-    mov eax, 0x07c0
-    mov ds, eax
     mov eax, 0xb800
     mov es, eax
     mov eax, 0 ; set eax to 0 ->i=0
@@ -23,14 +21,13 @@ section code
 
     jl .clear
 
-mov eax, text1
-mov ecx, 3*2*80
-push .end
+mov eax, welcome
+mov ecx, 0*2*80
 call .print
 
-.end:
-    mov byte [es:0x00], 'L'
-    jmp $
+jmp .switch
+
+
 
 .print:
     mov dl, byte [eax + ebx]
@@ -48,10 +45,56 @@ call .print
 .print_end:
     mov eax, 0
     ret
+.switch:
+    cli ; Turn off the interrupt
+    lgdt [gdt_descriptor] ;Load the GDT table
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax; make the switch
+    jmp protected_start
 
+welcome: db 'Welcome To PhoenixOS',0
 
-text: db 'Hello World', 0
-text1: db 'This is another text', 0
+[bits 32]
+protected_start:
+    mov ax, data_seg
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    ;update the stack pointer
+    mov ebp, 0x90000
+    mov esp, ebp
+
+    jmp $
+
+gdt_begin:
+gdt_null_descriptor:
+    dd 0x00
+    dd 0x00
+gdt_code_seg:
+    dw 0xffff
+    dw 0x00
+    db 0x00
+    db 10011010b
+    db 11001111b
+    db 0x00
+gdt_data_seg:
+    dw 0xffff
+    dw 0x00
+    db 0x00
+    db 10010010b
+    db 11001111b
+    db 0x00
+gdt_end:
+gdt_descriptor:
+    dw gdt_end-gdt_begin-1
+    dd gdt_begin
+
+code_seg equ gdt_code_seg - gdt_begin
+data_seg equ gdt_data_seg - gdt_begin
 
 times 510 - ($-$$) db 0x00 ; pads the file with 0s, making it right size
 
